1659e91c84cc1ccf6cb81fd6265a54d0ea5833d0
huihudk4333 <forrealroboffical@gmail.com>
2026-07-18 20:37:04 +1000

    Update ready.js

--- ticket-bot/ticket-bot/events/ready.js
+++ ticket-bot/ticket-bot/events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`✅ Logged in as ${client.user.tag}`);
-    client.user.setActivity('for /ticket-panel', { type: 3 }); // 3 = Watching
+    client.user.setActivity('made and hosted by forrealrob dm him', { type: 3 }); // 3 = Watching
   },
 };
